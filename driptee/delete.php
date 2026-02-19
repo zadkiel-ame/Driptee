f3302a341fa957ae9f41390df12f568eebeb42d7
Joseph Macapagal <macapagalj05@gmail.com>
2026-02-20 02:39:03 +0800

    Delete driptee directory

--- driptee/delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'db.php';
-if(isset($_GET['id'])) {
-    $id = intval($_GET['id']);
-    $conn->query("DELETE FROM products WHERE id = $id");
-}
-header("Location: admin_dashboard.php");
-exit();
-?>
